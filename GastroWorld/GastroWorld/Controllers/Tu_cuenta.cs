5968cbafba5394f4a559e86c6d853d9929eaf314
sebas <juansanchezrico48@gmail.com>
2025-03-07 11:42:54 -0500

    cambios

---
+++ GastroWorld/GastroWorld/Controllers/Tu_cuenta.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GastroWorld.Controllers
+{
+    public class Tu_cuentaController : Controller
+    {
+        public IActionResult Tu_cuenta()
+        {
+            return View("Tu_cuenta");
+        }
+    }
+}

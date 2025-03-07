e9c3cdb8ed5141e31c03aef80d991774adc95c61
sebas <juansanchezrico48@gmail.com>
2025-03-07 11:42:54 -0500

    cambios

---
+++ GastroWorld/GastroWorld/Controllers/PgprincipalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GastroWorld.Controllers
+{
+    public class PgprincipalController : Controller
+    {
+        public IActionResult cargarView()
+        {
+            return View("cargarView");
+        }
+    }
+}
---
+++ GastroWorld/GastroWorld/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GastroWorld.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
